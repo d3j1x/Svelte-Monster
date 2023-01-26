f7db19de4c3b666cbc66c835184eb5435406801d
d3j1x <d3j1x@yahoo.com>
2023-01-26 10:55:06 +0100

    Component & props

---
+++ src/lib/006/firstComponent.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let name = 'svelte is amazing';
+    export let email = "example@gmail.com"
+</script>
+
+
+<h4 style="text-align: center;color:blue">First component</h4>
+
+<div>
+    Component : {name} - {email}
+</div>
---
+++ src/routes/006/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+    import First from '$lib/006/firstComponent.svelte';
+    let name = 'world';
+    const obj ={
+        name: 'Shiheb',
+        email: 'lihau@lihau.com'
+    }
+    const target = {
+        ...obj
+    }
+
+</script>
+
+<h4 style="text-align: center;color:blue">Component and props</h4>
+<h1>Hello {name}!</h1>
+
+<hr/>
+<First name={name}/>
+<First {name}/>
+<First name="haha"/>
+<First />
+<First name={obj.name} email={obj.email}/>
+<First {...obj} />
+
+<hr />
+<div class="links">
+    <a href="/">HOME</a>
+</div>
