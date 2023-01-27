2690ba3d0331b0fac1f21291fc642204404fa219
d3j1x <d3j1x@yahoo.com>
2023-01-27 17:15:07 +0100

    Bind:This

---
+++ src/lib/012/firstComponent.svelte
@@ -0,0 +1,16 @@
+<svelte:options accessors/>
+<script>
+    let count = 0;
+
+    export function resett(){
+        count = 0;
+    }
+    function inc(){
+        count ++;
+    }
+</script>
+
+<h4 style="text-align: center;color:blue">
+    First Component</h4>
+
+<button on:click={inc}>{count}</button>
---
+++ src/routes/012/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+    import First from'$lib/012/firstComponent.svelte';
+    import { onMount } from 'svelte';
+    let element;
+
+    onMount(() => {
+        //console.log(typeof element);
+        console.log(element);
+    })
+
+    function reset() {
+        element.value = '';
+    }
+
+    //Component
+    let option;
+    function resett() {
+        //console.log(option.count);
+        option.resett();
+    }
+</script>
+
+<h4 style="text-align: center;color:blue">
+    Bind This</h4>
+
+<input bind:this={element}/>
+<hr />
+<button on:click={reset}>Reset</button>
+
+
+<hr />
+<First  bind:this={option}/>
+<hr />
+<button on:click={resett}>Resett</button>
+
+
+<hr />
+<div class="links">
+    <a href="/">HOME</a>
+</div>
