b6acf4a2d1b55e889f1a2e243c1f82366e8341a6
d3j1x <d3j1x@yahoo.com>
2023-01-27 11:22:21 +0100

    Binding

---
+++ src/lib/010/firstComponent.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let count = 0;
+    import {createEventDispatcher} from 'svelte';
+    const dispatch = createEventDispatcher();
+    $: dispatch('countChanged', count)
+</script>
+
+<h4 style="text-align: center;color:blue">First Component</h4>
+
+
+<button on:click={()=> {count ++;}}>Increment</button>
+<div>
+    count: {count}
+</div>
---
+++ src/routes/010/+page.svelte
@@ -0,0 +1,51 @@
+<script>
+    import First from '$lib/010/firstComponent.svelte';
+    let value = '';
+    let foo = '';
+    let count;
+
+    function onInput(event) {
+        value = this.value;
+    }
+</script>
+
+<h4 style="text-align: center;color:blue">Binding</h4>
+
+
+<input on:input={onInput} {value} />
+
+<div>value: "{value}"</div>
+
+<button on:click={()=> value = "Click!"}>Click Me</button>
+<br/>
+<hr/>
+<br/>
+<input bind:value={foo} />
+<div>foo: "{foo}"</div>
+
+
+<button on:click={()=> foo = "Click!"}>Click Me</button>
+
+<hr/>
+<First on:countChanged={(event)=>{count = event.detail}} 
+    {count}/>
+
+<button on:click={() => {count = 999}}>
+    Click Me
+</button>
+<hr/>
+
+
+<First 
+    bind:count
+    />
+
+<button on:click={() => {count = 999}}>
+    Click Me
+</button>
+
+
+<hr />
+<div class="links">
+    <a href="/">HOME</a>
+</div>
