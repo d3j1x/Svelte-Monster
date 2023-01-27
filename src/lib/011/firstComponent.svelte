14cdbc8ba0a41ac8228ecfcffc8b84fd5145aebc
d3j1x <d3j1x@yahoo.com>
2023-01-27 16:27:35 +0100

    Bind:Group

---
+++ src/lib/011/firstComponent.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let option ='3';
+</script>
+
+<h4 style="text-align: center;color:blue">First Component</h4>
+
+
+<button on:click={()=> {
+    option = '4';
+}}>
+reset</button>
+
+<label>
+    <input type="radio" name="com" bind:group={option} 
+    value="1">
+    1
+</label>
+
+<label>
+    <input type="radio" name="com" bind:group={option} 
+    value="2">
+    2
+</label>
+
+<label>
+    <input type="radio" name="com" bind:group={option} 
+    value="3">
+    3
+</label>
+
+<label>
+    <input type="radio" name="com" bind:group={option} 
+    value="4">
+    4
+</label>
+
+Selected Option : {option}
---
+++ src/routes/011/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+    import First from '$lib/011/firstComponent.svelte';
+    let flavours = ['3'];
+</script>
+
+
+
+<h4 style="text-align: center;color:blue">
+    Binding Group</h4>
+
+<button on:click={()=> {
+    flavours = ['2','3'];
+}}>
+    reset</button>
+
+<label>
+    <input type="checkbox" bind:group={flavours} value="1">
+    1
+</label>
+
+<label>
+    <input type="checkbox" bind:group={flavours} value="2">
+    2
+</label>
+
+<label>
+    <input type="checkbox" bind:group={flavours} value="3">
+    3
+</label>
+
+Flavours: {flavours.join(',') || '-'}
+
+
+
+<hr />
+<First />
+
+<hr />
+
+<div class="links">
+    <a href="/">HOME</a>
+</div>
