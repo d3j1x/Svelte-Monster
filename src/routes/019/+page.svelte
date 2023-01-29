876d634e74545ebea6352b5757d4e9292f90a174
d3j1x <d3j1x@yahoo.com>
2023-01-29 17:33:36 +0100

    Key Block

---
+++ src/routes/019/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+
+    import { fade } from 'svelte/transition';
+
+    let count =0;
+    let countK =0;
+</script>
+
+
+<h4 style="text-align: center;color:blue">
+    # # # Key Block # # #
+</h4>
+
+<input />
+<button on:click={() => count ++}>{count}</button>
+
+<hr />
+{#key countK}
+    <input />
+    <div in:fade>{countK}</div>
+{/key}
+<button on:click={() => countK ++}>{countK}</button>
+
+
+<hr />
+<div class="links">
+    <a href="/">HOME</a>
+</div>
