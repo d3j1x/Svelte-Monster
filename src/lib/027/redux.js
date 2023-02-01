d070e9486e85580f661f20893611a2bf35ad7f51
d3j1x <d3j1x@yahoo.com>
2023-02-01 15:14:45 +0100

    Redux store

---
+++ src/lib/027/redux.js
@@ -0,0 +1,15 @@
+import './patchProcess';
+import {legacy_createStore as createStore} from 'redux';
+
+function reducer (state = 0, action){
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1
+        case 'DECREMENT':
+            return state -1
+        default:
+            return state
+    }
+}
+
+export default createStore(reducer);
---
+++ src/routes/027/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+    import store from '$lib/027/redux.js';
+
+    let state = store.getState();
+
+    store.subscribe(() => {
+        state = store.getState();
+    });
+
+    function decrement() {
+        store.dispatch({type:'DECREMENT'});
+    }
+
+    function increment() {
+        store.dispatch({type:'INCREMENT'});
+    }
+</script>
+
+
+
+<h4 style="text-align: center;color:blue">
+    # # # Redux Store  # # #
+</h4>
+
+<button on:click={decrement}>-</button>
+{state}
+<button on:click={increment}>+</button>
+
+
+
+<hr />
+<div class="links">
+    <a href="/">HOME</a>
+</div>
