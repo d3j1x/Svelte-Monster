f10324969d12d90a02af84803dfd859ead9bfe4a
d3j1x <d3j1x@yahoo.com>
2023-02-02 14:38:54 +0100

    Valtio

---
+++ src/lib/028/data.js
@@ -0,0 +1,3 @@
+import {proxy} from "valtio/vanilla";
+
+export default proxy({count:0, text:"hello"});
---
+++ src/routes/028/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+    import { onMount } from "svelte";
+    import data from "$lib/028/data";
+    import {subscribe, snapshot} from "valtio/vanilla";
+
+    let _snapshot = snapshot(data);
+
+    onMount(() => {
+        return subscribe(data, ()=> {
+            _snapshot = snapshot(data);
+        });
+    });
+</script>
+
+
+
+<h4 style="text-align: center;color:blue">
+    # # # Valtio # # #
+</h4>
+
+<button on:click={()=> data.count--}>-</button>
+{_snapshot.count}
+<button on:click={()=> data.count++}>+</button>
+
+<input bind:value={data.text}/>
+{_snapshot.text}
+
+
+<hr />
+<div class="links">
+    <a href="/">HOME</a>
+</div>
