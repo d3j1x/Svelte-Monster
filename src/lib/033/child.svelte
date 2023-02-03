d0b1152cd46a6a621c0551c13ad7cfcde55cd6d0
d3j1x <d3j1x@yahoo.com>
2023-02-03 17:46:06 +0100

    DOM events as svelte store 2

---
+++ src/lib/033/child.svelte
@@ -0,0 +1,9 @@
+<script>
+    import mousePosition from '$lib/032/mousePosition';
+</script>
+
+<h4 style="text-align: center;color:blue">
+    # # # Child Component # # #
+</h4>
+
+[Children] Mouse position: {$mousePosition.x},{$mousePosition.y}
---
+++ src/routes/033/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import mousePosition from '$lib/033/mousePosition';
+    import Child from '$lib/033/child.svelte';
+</script>
+
+<h4 style="text-align: center;color:blue">
+    # # # DOM events as svelte store 2 # # #
+</h4>
+
+Mouse position: {$mousePosition.x},{$mousePosition.y}
+
+<hr />
+
+<Child />
+
+<hr />
+<div class="links">
+    <a href="/">HOME</a>
+</div>
