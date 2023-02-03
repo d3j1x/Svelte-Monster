be7c3b7210cd7a3d4b5f72a4a40df7addf288a48
d3j1x <d3j1x@yahoo.com>
2023-02-03 16:59:57 +0100

    Dom event

---
+++ src/routes/031/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+    import { onMount } from 'svelte';
+    let x = 0, y = 0;
+
+    onMount(()=>{
+        function move(event){
+            x = event.clientX;
+            y = event.clientY;
+        }
+        document.body.addEventListener("mousemove",move);
+        return () => {
+            document.body.addEventListener("mousemove", move);
+        }
+    });
+    $: console.log(x, y);
+</script>
+
+
+
+<h4 style="text-align: center;color:blue">
+    # # # DOM events # # #
+</h4>
+
+Mouse position: {x},{y}
+
+
+<hr />
+<div class="links">
+    <a href="/">HOME</a>
+</div>
