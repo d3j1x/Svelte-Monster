b4bd1d6d64c7421614729881594df5f58aefbad5
Shiheb Din شهاب الدين <d3j1x@yahoo.com>
2023-02-07 10:33:28 +0000

    Tweened svelte store

---
+++ src/routes/036/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+    import {tweened} from 'svelte/motion';
+
+    //let value = tweened(0);
+    let value = tweened(0, {duration: 5000});
+    //let value = tweened(0, {duration: 5000, delay: 2000});
+</script>
+
+
+
+<h4 style="text-align: center;color:blue">
+    # # # Tweened svelte store # # #
+</h4>
+
+<button on:click={() => {$value-=10}}>-</button>
+<div>
+    {$value}
+</div>
+<button on:click={() => {$value+=10}}>+</button>
+
+
+
+<hr />
+<div class="links">
+    <a href="/">HOME</a>
+</div>
