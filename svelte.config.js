9be22c75a54aa768872b6905feb3e5d4f7e783d6
Shiheb Din شهاب الدين <d3j1x@yahoo.com>
2023-02-10 13:12:21 +0000

    adapter node & render config

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		adapter: adapter()
+// 	}
+// };
+
+// export default config;
 
 /** @type {import('@sveltejs/kit').Config} */
+
+import node from '@sveltejs/adapter-node';
+
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+
+		methodeOverride: {
+			allowed: ['PATCH', 'DELETE']
+		}
 	}
 };
 
