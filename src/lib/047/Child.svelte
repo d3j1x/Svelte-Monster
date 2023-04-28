d9343280d753a8f8995762116e170eed8f401eb9
Smrt Mrkt السوق الذكية <smrtmrket@gmail.com>
2023-04-28 16:53:00 +0000

    Store vs Context

---
+++ src/lib/047/Child.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { getContext } from "svelte";
+    import { data as storeData } from './store';
+
+    const contexData = getContext('data');
+</script>
+
+<div>
+    Context: {$contexData}, Store: {$storeData}
+</div>
---
+++ src/routes/047/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Parent1 from '$lib/047/Parent1.svelte';
+    import Parent2 from '$lib/047/Parent2.svelte';
+</script>
+
+<Parent1 />
+
+<hr />
+
+<Parent2 />
