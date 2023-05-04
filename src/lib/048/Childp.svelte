2dd36bb3b2f8eb375e22b2fed568075ef8209bfd
Smrt Mrkt السوق الذكية <smrtmrket@gmail.com>
2023-05-04 16:54:36 +0000

    Intro to Svelte Action

---
+++ src/lib/048/Childp.svelte
@@ -0,0 +1,17 @@
+<script>
+	let name = 'world';
+	
+	function lihauAction(node, params) {
+		console.log(node,params);
+// 		node.innerHTML = params;
+		return {
+			destroy() {
+				console.log('node is destroyed')
+			}
+		}
+	}
+</script>
+
+<h1 use:lihauAction={name}>Hello {name}!</h1>
+
+<h2>Hello {name}!</h2>
