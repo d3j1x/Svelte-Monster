b7c3e62902a79f3aba1076b6c364d6967da588aa
Smrt Mrkt السوق الذكية <smrtmrket@gmail.com>
2023-05-09 17:36:53 +0000

    Reusing event listeners with Svelte Actions 3

---
+++ src/lib/054/Result1.svelte
@@ -0,0 +1,33 @@
+<script>
+	let input1,input2,input3;
+	
+
+	
+	function nextOnEnterAction(element, nextElementToFocus) {
+		function nextOnEnter(event) {
+			if (event.key === 'Enter') {
+				if (nextElementToFocus) {
+					nextElementToFocus.focus();
+				} else {
+					event.currentTarget.blur();
+				}
+			}
+		}
+		element.addEventListener('keydown', nextOnEnter);
+		
+		return {
+			update(nextNextElementToFocus){
+				nextElementToFocus = nextNextElementToFocus;
+			},
+			destroy() {
+				element.removeEventListener('keydown', nextOnEnter);
+			}
+		}
+	}
+	
+</script>
+
+<input bind:this={input1} use:nextOnEnterAction={input2} />
+<input bind:this={input2} use:nextOnEnterAction={input3} />
+<input bind:this={input3} use:nextOnEnterAction />
+
