780450fbecffc5d40aa69f569a77f5502b2f0278
Smrt Mrkt السوق الذكية <smrtmrket@gmail.com>
2023-05-18 13:14:05 +0000

    Reusing event listeners with Svelte Actions 4

---
+++ src/lib/055/Example.svelte
@@ -0,0 +1,29 @@
+<script>
+	let unlocked = false;
+	let keys = [];
+	let timeoutId = null;
+	
+	let SECRET = 'ArrowUp,ArrowDown,ArrowUp,ArrowDown,ArrowLeft,ArrowRight,ArrowLeft,ArrowRight';
+	
+	function onKeydown(event) {
+		keys.push(event.key);
+		
+		if (keys.join(',') === SECRET) {
+			unlocked = true;
+		}
+		
+		if (timeoutId) clearTimeout(timeoutId);
+		
+		timeoutId = setTimeout(() => {
+			timeoutId = null;
+			keys = [];
+		}, 1000);
+	}
+	
+</script>
+
+<input on:keydown={onKeydown} />
+
+{#if unlocked}
+	<div>Secret Unlocked!</div>
+{/if}
