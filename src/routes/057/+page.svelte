3b07fd09ee84769c9d865d350c9d5b0121d6efdd
Smrt Mrkt السوق الذكية <smrtmrket@gmail.com>
2023-05-22 12:13:42 +0000

    The Order of Svelte Actions

---
+++ src/routes/057/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+	let valueBeforeAction = '';
+	let valueAfterAction = '';
+	
+	function uppercase(element) {
+		function onInput(event) {
+			element.value = element.value.toUpperCase();
+		}
+		element.addEventListener('input', onInput);
+		return {
+			destroy() {
+				element.removeEventListener('input', onInput);
+			}
+		}
+	}
+</script>
+
+<h1>The Order of Svelte Actions</h1>
+<hr />
+
+<input
+	bind:value={valueBeforeAction}	
+	use:uppercase
+/>
+
+<input
+	use:uppercase
+	bind:value={valueAfterAction}
+/>
+
+<div>{valueBeforeAction}</div>
+<div>{valueAfterAction}</div>
