a65391e52c5c12b7f65496e1045cd32f1b6074a9
Smrt Mrkt السوق الذكية <smrtmrket@gmail.com>
2023-05-25 15:50:20 +0000

    use : clickOutside

---
+++ src/routes/058/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+	function clickOutside(element, callbackFunction) {
+		function onClick(event) {
+			if (!element.contains(event.target)) {
+				callbackFunction();
+			}
+		}
+		document.body.addEventListener('click', onClick);
+		return {
+			update(newCallBackFunction) {
+				callbackFunction = newCallBackFunction;
+			},
+			destroy() {
+				document.body.removeEventListener('click', onClick);
+			}
+		}
+	}
+</script>
+
+<h1> use : clickOutside </h1>
+<hr />
+
+<div class="modal" 
+		 use:clickOutside={() => {console.log('Clicked outside')}}>
+	Some Modal
+</div>
+
+<style>
+	.modal {
+		padding: 16px;
+		border: 1px solid black;
+	}
+</style>
