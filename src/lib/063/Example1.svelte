1efc9e9380fb336cea6630259f7d79fde423a776
Smrt Mrkt السوق الذكية <smrtmrket@gmail.com>
2023-06-01 11:31:57 +0000

    Ensemble Actions

---
+++ src/lib/063/Example1.svelte
@@ -0,0 +1,91 @@
+<script>
+	let dropzone1 = '';
+	let dropzone2 = '';
+	
+	let canDrop1 = false;
+	let canDrop2 = false;
+	
+	function onDragStart(event) {
+		const text = event.target.innerText;
+		event.dataTransfer.setData('text',text);
+		canDrop1 = text !== 'C';
+		canDrop2 = text !== 'A';
+	}
+	
+	function onDragEnd(event) {
+		canDrop1 = canDrop2 = false;
+	}
+	
+	function onDrop1(event) {
+		event.preventDefault();
+		const data = event.dataTransfer.getData("text");
+		dropzone1 += data;
+	}
+	
+	function onDrop2(event) {
+		event.preventDefault();
+		const data = event.dataTransfer.getData("text");
+		dropzone2 += data;
+	}
+	
+	function onDragOver1(event) {
+		if (canDrop1) {
+			event.preventDefault();
+		}
+	}
+	
+	function onDragOver2(event) {
+		if (canDrop2) {
+			event.preventDefault();
+		}
+	}
+	
+</script>
+
+
+<div style="display: flex;">
+	
+	<div class="dropzone" on:drop={onDrop1} on:dragover={onDragOver1}>
+		{dropzone1}</div>
+	
+	<div class="dropzone" on:drop={onDrop2} on:dragover={onDragOver2}>
+		{dropzone2}</div>
+	
+</div>
+
+
+<div style="display: flex;">
+	
+		<div class="draggable" draggable="true" on:dragstart={onDragStart} on:dragend={onDragEnd}>
+			A</div>
+	
+		<div class="draggable" draggable="true" on:dragstart={onDragStart} on:dragend={onDragEnd}>
+			B</div>
+	
+		<div class="draggable" draggable="true" on:dragstart={onDragStart} on:dragend={onDragEnd}>
+			C</div>
+	
+</div>
+
+
+<style>
+
+	.dropzone {
+		width: 100px;
+		height: 100px;
+		background: lightgrey;
+		border: solid 2px;
+		margin: 10px;
+	}
+	
+	.draggable {
+		background: blue;
+		width: 30px;
+		height: 30px;
+		border-radius:50%;
+		color:white;
+		display:grid;
+		place-items: center;
+	}
+	
+</style>
---
+++ src/routes/063/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Example1 from '$lib/063/Example1.svelte';
+	import Example1Final from '$lib/063/Example1Final.svelte';
+</script>
+
+<h1>Ensemble actions</h1>
+<hr />
+
+<h2>Example1</h2>
+<Example1 />
+
+<hr />
+
+<h2>Example1Final</h2>
+<Example1Final />
+
+<hr />
+
