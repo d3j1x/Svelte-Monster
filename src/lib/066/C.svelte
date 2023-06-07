5d8678f287cf45b0dd24663d7308c237a6a6b6ef
Smrt Mrkt السوق الذكية <smrtmrket@gmail.com>
2023-06-07 11:32:29 +0000

    SLOTs Forwarding

---
+++ src/lib/066/C.svelte
@@ -0,0 +1,16 @@
+<script>
+	let counter = 0;
+</script>
+
+<button on:click={()=> counter++}>C: {counter}</button>
+<br />
+
+C Header: <slot name="header" {counter}/>
+
+<!-- C Header: <slot name="header" /> -->
+
+<div>
+	C: <slot name="content" />
+</div>
+
+C Footer: <slot name="footer" />
---
+++ src/routes/066/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import A from '$lib/066/A.svelte';
+</script>
+
+<h1>SLOTs Forwarding</h1>
+<hr />
+
+<A />
