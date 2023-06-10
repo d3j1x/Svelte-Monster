c04448e9ac1f57245c9f72a5467d658d6a67a092
Smrt Mrkt السوق الذكية <smrtmrket@gmail.com>
2023-06-10 09:49:09 +0000

    $$SLOTS

---
+++ src/lib/067/A.svelte
@@ -0,0 +1,21 @@
+<div>
+	A: <slot name='a'>Fallback for a</slot>
+</div>
+
+<!-- <div>
+	B: <slot name='b'>Fallback for b</slot>
+</div> -->
+
+<!-- <div>
+	B: <slot name='b'></slot>
+</div> -->
+
+{#if $$slots.b}
+	<div>
+		B: <slot name='b'></slot>
+	</div>
+{/if}
+
+<div>
+	C: <slot name='c'>Fallback for c</slot>
+</div>
---
+++ src/routes/067/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+	import A from '$lib/067/A.svelte';
+</script>
+
+<h1>$$SLOTs</h1>
+
+<A />
+
+<hr />
+
+<A>
+	<div slot="a">something for a</div>
+	<div slot="b">something for b</div>
+	<div slot="c">something for c</div>
+</A>
+
+<hr />
+
+<A>
+	<div slot="a">something for a</div>
+	<div slot="b">something for b</div>
+</A>
+
+<hr />
+
+<A>
+	<div slot="a">something for a</div>
+	<svelte:fragment slot="b"></svelte:fragment>
+	<div slot="c">something for c</div>
+</A>
+
+<hr />
+
+<A>
+	<div slot="a">something for a</div>
+	<svelte:fragment slot="b">{#if true}sss{/if}</svelte:fragment>
+	<div slot="c">something for c</div>
+</A>
