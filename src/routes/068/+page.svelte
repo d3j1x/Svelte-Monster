508515a973d34a9249b8e9406ba03852b3d8755e
Smrt Mrkt السوق الذكية <smrtmrket@gmail.com>
2023-06-10 14:30:32 +0000

    Infinite List

---
+++ src/routes/068/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+	import List from '$lib/068/List.svelte';
+	import data from '$lib/068/data';
+</script>
+
+<h1>Infinite List</h1>
+<hr />
+
+<List 
+			{...$data}
+			key='id'
+			let:item={{ download_url, author }}
+			on:loadMore={data.fetchMore}
+			>
+	
+<!-- 	<img 
+			 style="width:100%"
+			 alt="stunning viewtaken by {author}" 
+			 src={download_url}> -->
+	
+	<img 
+			 slot="item"
+			 style="width:100%"
+			 alt="stunning viewtaken by {author}" 
+			 src={download_url}>
+
+</List>
+
+
+<!-- 16:01
+ -->
