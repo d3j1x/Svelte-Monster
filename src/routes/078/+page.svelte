c23ac1eb9f16330197e47164d72aa015e3b431e0
Smrt Mrkt السوق الذكية <smrtmrket@gmail.com>
2023-06-15 10:31:59 +0000

    <svelte:self />

---
+++ src/routes/078/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	let data = [1,2,3,4,5];
+</script>
+
+<h1>svelte:self</h1>
+<hr />
+
+<span class="list">
+	{#each data as item}
+		<span>{item}</span>
+	{/each}
+</span>
+
+<style>
+	span {
+		display: inline-block;
+		border: 1px solid red;
+		margin: 4px;
+	}
+	
+	.list {
+		border-color: blue
+	}
+</style>
---
+++ src/routes/079/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Component from '$lib/079/Component.svelte';
+</script>
+
+<h1>
+	svelte:self 2
+</h1>
+
+<Component />
