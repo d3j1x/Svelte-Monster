e007c0aba2a0cb4ef6197c70d54bf96ef6b09a62
Smrt Mrkt السوق الذكية <smrtmrket@gmail.com>
2023-07-13 12:11:12 +0000

    @html

---
+++ src/routes/096/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+	import {onMount, tick} from 'svelte';
+	let html = `<b>this is a bold text</b><button id="xyz">ClickMe</button>`;
+
+	function onClick(event) {
+		console.log(event.target.id);
+	}
+
+	let div;
+	
+	// onMount(() => {
+	// 	div.querySelector('button').addEventListener('click', () => {
+	// 		console.log('click!');
+	// 	});
+	// })
+
+	// $: html && attachButtonListener();
+	$: html, attachButtonListener();
+
+	async function attachButtonListener() {
+		// if(!div) return
+		console.log("attachButtonListener");
+		await tick();
+		
+		div.querySelector('button').addEventListener('click', () => {
+			console.log('click!');
+		});
+	}
+
+	
+</script>
+
+<h1>@html3</h1>
+
+<textarea bind:value={html} cols="40" rows="4" />
+
+<div bind:this={div}>
+	{@html html}
+</div>
